c59833614c55312785119715d87fda983de8ea69
Rodolfo <rodolvg87@gmail.com>
2023-06-10 17:32:40 +0200

    Ajuste de base de datos

--- db/nima_db.sql
+++ db/nima_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-06-2023 a las 16:19:08
+-- Tiempo de generación: 10-06-2023 a las 17:31:47
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 7.4.33
 
@@ -24,13 +24,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `botones`
+-- Estructura de tabla para la tabla `boton`
 --
 
-CREATE TABLE `botones` (
-  `id` int(11) NOT NULL,
-  `name` varchar(250) NOT NULL
+CREATE TABLE `boton` (
+  `id_boton` int(11) NOT NULL,
+  `imagen` varchar(250) NOT NULL,
+  `descripcion` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `boton`
+--
+
+INSERT INTO `boton` (`id_boton`, `imagen`, `descripcion`) VALUES
+(1, 'blanco 1', 'blanco 1');
 
 -- --------------------------------------------------------
 
@@ -39,9 +47,17 @@ CREATE TABLE `botones` (
 --
 
 CREATE TABLE `cuello` (
-  `id` int(11) NOT NULL,
-  `name` varchar(250) NOT NULL
+  `id_cuello` int(11) NOT NULL,
+  `imagen` varchar(250) NOT NULL,
+  `descripcion` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cuello`
+--
+
+INSERT INTO `cuello` (`id_cuello`, `imagen`, `descripcion`) VALUES
+(1, 'cuello 1', 'cuello 1');
 
 -- --------------------------------------------------------
 
@@ -50,9 +66,17 @@ CREATE TABLE `cuello` (
 --
 
 CREATE TABLE `cuerpo` (
-  `id` int(11) NOT NULL,
-  `name` varchar(250) NOT NULL
+  `id_cuerpo` int(11) NOT NULL,
+  `imagen` varchar(250) NOT NULL,
+  `descripcion` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cuerpo`
+--
+
+INSERT INTO `cuerpo` (`id_cuerpo`, `imagen`, `descripcion`) VALUES
+(1, 'cuerpo 1', 'cuerpo 1');
 
 -- --------------------------------------------------------
 
@@ -61,136 +85,159 @@ CREATE TABLE `cuerpo` (
 --
 
 CREATE TABLE `manga` (
-  `id` int(11) NOT NULL,
-  `name` varchar(250) NOT NULL
+  `id_manga` int(11) NOT NULL,
+  `imagen` varchar(250) NOT NULL,
+  `descripcion` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `manga`
+--
+
+INSERT INTO `manga` (`id_manga`, `imagen`, `descripcion`) VALUES
+(1, 'manga 1', 'manga 1');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `productos`
+-- Estructura de tabla para la tabla `producto`
 --
 
-CREATE TABLE `productos` (
-  `id` int(11) NOT NULL,
-  `name` varchar(250) NOT NULL
+CREATE TABLE `producto` (
+  `id_producto` int(11) NOT NULL,
+  `imagen` varchar(250) NOT NULL,
+  `descripcion` varchar(250) NOT NULL,
+  `id_boton` int(11) NOT NULL,
+  `id_tela` int(11) NOT NULL,
+  `id_cuerpo` int(11) NOT NULL,
+  `id_cuello` int(11) NOT NULL,
+  `id_manga` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id_producto`, `imagen`, `descripcion`, `id_boton`, `id_tela`, `id_cuerpo`, `id_cuello`, `id_manga`) VALUES
+(1, '1111', 'Blusa de escote grande...', 1, 1, 1, 1, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `telas`
+-- Estructura de tabla para la tabla `tela`
 --
 
-CREATE TABLE `telas` (
-  `id` int(11) NOT NULL,
-  `name` varchar(250) NOT NULL
+CREATE TABLE `tela` (
+  `id_tela` int(11) NOT NULL,
+  `imagen` varchar(250) NOT NULL,
+  `descripcion` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Estructura de tabla para la tabla `usuario`
+-- Volcado de datos para la tabla `tela`
 --
 
-CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL,
-  `name` varchar(250) NOT NULL,
-  `email` varchar(250) NOT NULL,
-  `password` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `tela` (`id_tela`, `imagen`, `descripcion`) VALUES
+(1, 'crepe-arena', 'crepe-arena');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `botones`
+-- Indices de la tabla `boton`
 --
-ALTER TABLE `botones`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `boton`
+  ADD PRIMARY KEY (`id_boton`);
 
 --
 -- Indices de la tabla `cuello`
 --
 ALTER TABLE `cuello`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_cuello`);
 
 --
 -- Indices de la tabla `cuerpo`
 --
 ALTER TABLE `cuerpo`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_cuerpo`);
 
 --
 -- Indices de la tabla `manga`
 --
 ALTER TABLE `manga`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_manga`);
 
 --
--- Indices de la tabla `productos`
+-- Indices de la tabla `producto`
 --
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `Refboton2` (`id_boton`),
+  ADD KEY `Reftela3` (`id_tela`),
+  ADD KEY `Refcuello4` (`id_cuello`),
+  ADD KEY `Refcuerpo5` (`id_cuerpo`),
+  ADD KEY `Refmanga6` (`id_manga`);
 
 --
--- Indices de la tabla `telas`
+-- Indices de la tabla `tela`
 --
-ALTER TABLE `telas`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `tela`
+  ADD PRIMARY KEY (`id_tela`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `botones`
+-- AUTO_INCREMENT de la tabla `boton`
 --
-ALTER TABLE `botones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `boton`
+  MODIFY `id_boton` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `cuello`
 --
 ALTER TABLE `cuello`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_cuello` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `cuerpo`
 --
 ALTER TABLE `cuerpo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_cuerpo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `manga`
 --
 ALTER TABLE `manga`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_manga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de la tabla `productos`
+-- AUTO_INCREMENT de la tabla `producto`
 --
-ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `producto`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de la tabla `telas`
+-- AUTO_INCREMENT de la tabla `tela`
 --
-ALTER TABLE `telas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tela`
+  MODIFY `id_tela` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de la tabla `usuario`
+-- Restricciones para tablas volcadas
 --
-ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `Refboton2` FOREIGN KEY (`id_boton`) REFERENCES `boton` (`id_boton`),
+  ADD CONSTRAINT `Refcuello4` FOREIGN KEY (`id_cuello`) REFERENCES `cuello` (`id_cuello`),
+  ADD CONSTRAINT `Refcuerpo5` FOREIGN KEY (`id_cuerpo`) REFERENCES `cuerpo` (`id_cuerpo`),
+  ADD CONSTRAINT `Refmanga6` FOREIGN KEY (`id_manga`) REFERENCES `manga` (`id_manga`),
+  ADD CONSTRAINT `Reftela3` FOREIGN KEY (`id_tela`) REFERENCES `tela` (`id_tela`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
