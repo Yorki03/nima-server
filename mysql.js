ff541bb723521d4907cbfd38a8a740000d10dbd8
Yorki03 <110152632+Yorki03@users.noreply.github.com>
2023-06-29 02:35:41 +0200

    actualización de la pagina

--- mysql.js
+++ mysql.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const mysqlConnet = mysql.createConnection({
-    host: 'bja1sncwxsgkzisv2qnd-mysql.services.clever-cloud.com',
-    user: 'ub74hk6vch7oojac',
-    password: 'C02qXVJ4q9sjuvuzV7BC',
-    database: 'bja1sncwxsgkzisv2qnd'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nima_db'
 });
 
 mysqlConnet.connect(function(err){
